58ef996427e1fdcd197eb439e8336f7d5e437f25
Joakim Vu <joakimvu@icloud.com>
2024-09-11 19:14:11 +0200

    Create pateint.actions.ts

---
+++ lib/actions/pateint.actions.ts
@@ -0,0 +1,24 @@
+"use server";
+
+import { users } from "../appwrite.config";
+import { ID, Query } from "node-appwrite";
+
+export const createUser = async (user: CreateUserParams) => {
+  console.log(JSON.stringify(user));
+  try {
+    const newUser = await users.create(
+      ID.unique(),
+      user.email,
+      user.phone,
+      undefined,
+      user.name
+    );
+    console.log({ newUser });
+  } catch (error: any) {
+    if (error && error?.code === 409) {
+      const documents = await users.list([Query.equal("email", [user.email])]);
+
+      return documents?.users[0];
+    }
+  }
+};
