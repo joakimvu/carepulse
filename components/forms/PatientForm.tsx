3cae37418f987f894ac5dd02ccee29fcf9f8f72c
Joakim Vu <joakimvu@icloud.com>
2024-09-11 19:14:09 +0200

    Update PatientForm.tsx

--- components/forms/PatientForm.tsx
+++ components/forms/PatientForm.tsx
@@ -9,6 +9,7 @@ import SumbitButton from "../SumbitButton";
 import { useState } from "react";
 import { UserFormValidation } from "@/lib/validation";
 import { useRouter } from "next/navigation";
+import { createUser } from "@/lib/actions/pateint.actions";
 
 export enum FormFieldType {
   INPUT = "input",
@@ -24,7 +25,6 @@ const PatientForm = () => {
   const router = useRouter();
   const [isLoading, setIsLoading] = useState(false);
 
-  // 1. Define your form.
   const form = useForm<z.infer<typeof UserFormValidation>>({
     resolver: zodResolver(UserFormValidation),
     defaultValues: {
@@ -34,28 +34,29 @@ const PatientForm = () => {
     },
   });
 
-  // 2. Define a submit handler.
   async function onSubmit({
     name,
     email,
     phone,
   }: z.infer<typeof UserFormValidation>) {
+    setIsLoading(true);
     try {
-      //   const userData = {
-      //     name,
-      //     email,
-      //     phone,
-      //   };
-      //   const user = await createUser(userData);
-      //   if (user) router.push(`/patients/${user.$id}/register`);
+      const userData = {
+        name,
+        email,
+        phone,
+      };
+      const user = await createUser(userData);
+      if (user) router.push(`/patients/${user.$id}/register`);
     } catch (error) {
       console.log(error);
     }
+    setIsLoading(false);
   }
 
   return (
     <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6 flex-1">
+      <form onSubmit={form.handleSubmit(onSubmit)} className="flex-1 space-y-6">
         <section className="mb-12 space-y-4">
           <h1 className="header">Hi there 👋</h1>
           <p className="text-dark-700">Schedule your first appointment</p>
