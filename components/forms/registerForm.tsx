85c6062972463ee3ddaba2329b627781eeb1c284
Joakim Vu <joakimvu@icloud.com>
2024-09-14 16:42:13 +0200

    Update registerForm.tsx

--- components/forms/registerForm.tsx
+++ components/forms/registerForm.tsx
@@ -299,11 +299,38 @@ const RegisterForm = ({ user }: { user: User }) => {
             label="Scanned copy of identification document"
             renderSkeleton={(field) => (
               <FormControl>
-                <FileUploader />
+                <FileUploader files={field.value} onChange={field.onChange} />
               </FormControl>
             )}
           />
         </div>
+
+        <section className="space-y-6">
+          <div className="mb-9 space-y-1">
+            <h2 className="sub-header">Concent and Privacy</h2>
+          </div>
+        </section>
+
+        <CustomFormField
+          fieldType={FormFieldType.CHECKBOX}
+          control={form.control}
+          name="treatmentConsent"
+          label="I consent to treatment"
+        />
+
+        <CustomFormField
+          fieldType={FormFieldType.CHECKBOX}
+          control={form.control}
+          name="disclosureConsent"
+          label="I consent to disclosure of information"
+        />
+
+        <CustomFormField
+          fieldType={FormFieldType.CHECKBOX}
+          control={form.control}
+          name="privacyConsent"
+          label="I consent to privacy policy"
+        />
 
         <SumbitButton isLoading={isLoading}>Get Started</SumbitButton>
       </form>
